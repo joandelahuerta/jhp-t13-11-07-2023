ac8bb411e23c70901be504586f4ea353b807d035
Joan de la Huerta <joandelahuertaplanas@gmail.com>
2023-07-11 16:56:14 +0200

    problema1 arreglado

---
+++ projects/problema1/src/app/cliente/cliente.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente',
+  templateUrl: './cliente.component.html',
+  styleUrls: ['./cliente.component.css']
+})
+export class ClienteComponent {
+
+  @Input() data:any;
+}
